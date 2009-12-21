95d779c075919cdf2544d4117b9a3592338c8f88
Mirko Vogt <dev@nanl.de>
2009-12-21 23:34:36 +0100

    add missing tests for tickets

--- test/functional/tickets_controller_test.rb
+++ test/functional/tickets_controller_test.rb
@@ -13,7 +13,22 @@ class TicketsControllerTest < ActionController::TestCase
   end
 
   test "create new ticket" do
-    #TODO: really create a ticket
+    assert_difference "Ticket.count", +1 do
+      post :create, :ticket => {
+        :name     => "standard",
+        :price    => 2342,
+        :comment  => "foobar fnord",
+        :custom   => true,
+      }
+    end
+
+    ticket = Ticket.last
+
+    assert_equal "standard", ticket.name
+    assert_equal 2342, ticket.price
+    assert_equal "foobar fnord", ticket.comment
+    assert_equal true, ticket.custom
+    assert_redirected_to tickets_path
   end
 
   test "get edit" do
@@ -21,13 +36,30 @@ class TicketsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "deleting a ticket type" do
-    #TODO: really delete the ticket
-    #assert_difference "Ticket.count", -1 do
-    #  delete :destroy, :id => 1
-    #end
+  test "update a ticket" do
+    put :update, :id => 1, :ticket => {
+        :name     => "standard",
+        :price    => 2342,
+        :comment  => "foobar fnord",
+        :custom   => true,
+    }
 
-    #assert_redirected_to tickets_path
+    assert_redirected_to tickets_path
+
+    ticket = Ticket.find(1)
+    assert_equal "standard", ticket.name
+    assert_equal 2342, ticket.price
+    assert_equal "foobar fnord", ticket.comment
+    assert_equal true, ticket.custom
+    assert_redirected_to tickets_path
+  end
+
+  test "deleting a ticket type" do
+    assert_difference "Ticket.count", -1 do
+      delete :destroy, :id => 1
+    end
+
+    assert_redirected_to tickets_path
   end
 
 end

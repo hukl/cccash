a4a48ad57c10d4bf7e4e31563c070d2b391215db
Mirko Vogt <dev@nanl.de>
2009-12-21 22:39:04 +0100

    add route for <tickets>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :cashboxes
   map.resources :groups
   map.resources :printers
+  map.resources :tickets
   map.admin   '/admin', :controller => :admin, :action => :index
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'

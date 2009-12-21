280e4dcf48182bcce6a5a11bd93cced11c5f851e
Mirko Vogt <dev@nanl.de>
2009-12-22 00:14:30 +0100

    add unit-tests or tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,5 @@
 class Ticket < ActiveRecord::Base
+  validates_presence_of     :name,  :price
+  validates_numericality_of :price, :greater_than_or_equal_to => 0
+  validates_uniqueness_of   :name
 end
--- test/unit/ticket_test.rb
+++ test/unit/ticket_test.rb
@@ -1,8 +1,28 @@
 require 'test_helper'
 
 class TicketTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "ticket must have a name" do
+    assert_raise ActiveRecord::RecordInvalid do
+      Ticket.create! :price => 0
+    end
+  end
+  
+  test "ticket must have a price" do
+    assert_raise ActiveRecord::RecordInvalid do
+      Ticket.create! :name => "foobar fnord"
+    end
+  end
+
+  test "ticket price must be greater or equal than zero" do
+    assert_raise ActiveRecord::RecordInvalid do
+      Ticket.create! :name => "foobar fnord", :price => "x"
+    end
+  end
+
+  test "ticket name must be unique" do
+    assert_raise ActiveRecord::RecordInvalid do
+      Ticket.create! :name => "Dummy ticket 1", :price => 2342
+    end
   end
 end

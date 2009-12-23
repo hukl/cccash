11042ae81504580f4750f9e2153fd75a906a5594
hukl <contact@smyck.org>
2009-12-23 08:48:02 +0100

    Unit tests for cart. See I told you first thing in the morning! ;)

--- test/functional/cart_controller_test.rb
+++ test/functional/cart_controller_test.rb
@@ -1,8 +1,34 @@
 require 'test_helper'
 
-class CartControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+class CartsControllerTest < ActionController::TestCase
+  
+  def setup
+    login_as :yesper
+  end
+  
+  test "cart has an empty tickets array" do
+    get :show
+    assert_response :success
+    assert_equal [], session[:cart].tickets
+  end
+  
+  test "adding a ticket to the cart" do
+    put :add_ticket_to, :id => tickets(:one).id
+    assert_response :success
+    assert_equal 1, session[:cart].tickets.size
+  end
+  
+  test "adding multiple tickets to the cart and delete one" do
+    get :show
+    
+    session[:cart].tickets << tickets(:one)
+    session[:cart].tickets << tickets(:two)
+    session[:cart].tickets << tickets(:one)
+    
+    delete :remove_ticket_from, :position => 1  # equals ticket :two in the cart
+    assert_response :success
+    assert_equal 2, session[:cart].tickets.size
+    assert_equal [1,1], session[:cart].tickets.map {|t| t.id} # 2x Dummy ticket 1
+    assert_select( "#cart table tr td", "Dummy ticket 1" )
   end
 end

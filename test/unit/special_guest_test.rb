10954eae78e15515e133c8d23b9d9ad7c797044e
hukl <contact@smyck.org>
2009-12-23 19:22:50 +0100

    @Mirko: model names = singular, controller names (if resources) = plural

--- test/unit/special_guest_test.rb
+++ test/unit/special_guest_test.rb
@@ -1,34 +1,34 @@
 require 'test_helper'
 
-class SpecialGuestsTest < ActiveSupport::TestCase
+class SpecialGuestTest < ActiveSupport::TestCase
 
   test "special guest must have at least a name or uid" do
     assert_raise ActiveRecord::RecordInvalid do
-      SpecialGuests.create!
+      SpecialGuest.create!
     end
   end
 
   test "do not fail when only uuid is given" do
-    SpecialGuests.create! :uid => "0xdeadbeef"
+    SpecialGuest.create! :uid => "0xdeadbeef"
   end
 
   test "do not fail when only name is given" do
-    SpecialGuests.create! :name => "Fnord"
+    SpecialGuest.create! :name => "Fnord"
   end
 
   test "do not fail when only forename is given" do
-    SpecialGuests.create! :forename => "Fnord"
+    SpecialGuest.create! :forename => "Fnord"
   end
 
   test "when uid is present then uid must be unique unless it's blank" do
     assert_raise ActiveRecord::RecordInvalid do
-      SpecialGuests.create! :uid => "blablub0xfnord"
+      SpecialGuest.create! :uid => "blablub0xfnord"
     end
   end
 
   test "when uid is blank do not fail when there is another blank uid" do
-    SpecialGuests.create! :name => "fnord1"
-    SpecialGuests.create! :name => "fnord2"
+    SpecialGuest.create! :name => "fnord1"
+    SpecialGuest.create! :name => "fnord2"
   end
 
 end

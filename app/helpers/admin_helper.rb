3a4891c8ef64a37686c3181931a7a76fb479a04c
hukl <contact@smyck.org>
2010-05-13 21:56:29 +0200

    fixed admin overview toggle link for workshifts

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,7 +1,13 @@
 module AdminHelper
   def toggle_activation_link(workshift)
+    if workshift.aasm_events_for_current_state.include?(:activate)
+      link_name = "Activate"
+    elsif workshift.aasm_events_for_current_state.include?(:deactivate)
+      link_name = "Deactivate"
+    end
+    
     link_to_remote(
-      (workshift.active? ? 'Deactivate' : 'Activate'),
+      link_name,
       :url => toggle_activation_workshift_path( workshift ),
       :method => :put
     )

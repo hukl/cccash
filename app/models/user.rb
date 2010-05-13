cc075865633702b0dcaa1870718e2be8b5d905ea
Simon Willmann <simon@villariba.basics.digital.udk-berlin.de>
2010-05-13 22:25:27 +0200

    adapted the user model to our new finite state machine

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ActiveRecord::Base
   include Authentication::ByCookieToken
 
   has_one   :workshift
-  has_many  :cleared_workshifts,
-            :class_name => "Workshift"
 
   validates_presence_of     :login
   validates_length_of       :login,    :within => 3..40
@@ -24,10 +22,12 @@ class User < ActiveRecord::Base
 
   named_scope :admins,  :conditions => { :admin => true }
   named_scope :angels,  :conditions => { :admin => false }
+
+  named_scope :cleared_workshifts, :conditions => { :state => 'cleared' }
   named_scope(
     :busy,
     :joins => :workshift,
-    :conditions => ["workshifts.user_id = users.id AND workshifts.cleared = false"]
+    :conditions => ["workshifts.user_id = users.id AND workshifts.state <> 'cleared'"]
   )
 
   # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
@@ -55,8 +55,8 @@ class User < ActiveRecord::Base
   end
   
   def workshifts
-    Workshift.all(
-      :conditions => {:user_id => id}, :order => "created_at desc")
+    Workshift.all( :conditions => {:user_id => id},
+                   :order      => "created_at desc")
   end
   
   def destroy
@@ -69,7 +69,7 @@ class User < ActiveRecord::Base
   end
   
   def active_workshift
-    Workshift.find(:first, :conditions => {:active => true, :user_id => id})
+    Workshift.find(:first, :conditions => ["state IN (?) AND user_id = ?", ["waiting_for_login", "active", "standby"], id])
   end
   
 end

ea38553a299a00ec7810572780dd7190cec675db
cashmaster <cashmaster@cashmaster.club.berlin.ccc.de>
2010-12-26 18:12:13 +0100

    Revert "Newer thinking sphinx version"
    This reverts commit f69f7879af34552e5b617702b1f42a02e837e3a9.

--- config/environment.rb
+++ config/environment.rb
@@ -23,7 +23,7 @@ Rails::Initializer.run do |config|
   config.gem(
     'thinking-sphinx',
     :lib     => 'thinking_sphinx',
-    :version => '1.3.20'
+    :version => '1.3.16'
   )
 
   config.gem(
